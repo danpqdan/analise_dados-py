4bb0280bb2fced21c672988284df733bfd87eccd
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:07:57 -0300

    [ADD] add definitions of main sampling types in 04_aula_tipos_amostras.py

---
+++ teorico-estatisticas/04_aula_tipos_amostras.py
@@ -0,0 +1,24 @@
+"""
+    Principais tipos de amostras:
+        - Aleatoria simples.
+        - Estratificada.
+        - Sistemática.
+        
+    * Amostra aleatoria simples:
+        - Um determinado número de elementos é retirado da população de forma aleatória.
+        - Todos os elementos da popupalação alvo do processo de amostragem, devem ter as
+        mesmas chances de serem selecionados para fazer parte da amostra.
+
+            * Com reposição
+                - Reposto apos amostragem
+            * Sem reposição
+                - Não é reposto apos passar por amostragem
+                
+    * Amostra estratificada:
+        - As vezes as populações estão dividas em grupos ou estratos.
+        
+    * Amostra sistematica:
+        - Nesse tipo de amostragem é escolhido um elemento aleátorio e apartir daí,
+        a cada N elementos um novo membro é escolhido.
+    
+"""
