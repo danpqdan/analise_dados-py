fb9b73b89d40828ca62eff56916933448e99a7c9
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:07:48 -0300

    [ADD] add exploratory data analysis concepts and definitions in 02_aula_EDA.py

---
+++ teorico-estatisticas/02_aula_EDA.py
@@ -0,0 +1,18 @@
+"""
+    Analise exploratória de dados:
+        Busca obter informações ocultas sobre os dados:
+            - Variação
+            - Anomalias
+            - Distribuição
+            - Tendências
+            - Padrões
+            - Relações
+            
+    EDA X Gráficos:
+        - Não são as mesmas coisas, porem EDA é altamente baseado na produção de gráficos.
+        - Gráficos de dispersão, boxplots, histogramas e ETCs
+    
+    Mundo real:
+        - O EDA faz parte do pipeline de qualquer processo de analise de dados, mesmo que informal
+    
+"""
