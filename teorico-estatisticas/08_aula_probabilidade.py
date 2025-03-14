ebe69d3d0669c09bd423774112aa433b54f85896
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:08:38 -0300

    add introduction to probability concepts in 08_aula_probabilidade.py

---
+++ teorico-estatisticas/08_aula_probabilidade.py
@@ -0,0 +1,36 @@
+"""
+    Probabilidade:
+        probabilidade é a chance de um evento ocorrer, é um número entre 0 e 1.
+        probabilidade(p): 0 <= p <= 1
+        P = 1: evento certo
+        P = 0: evento impossível
+        Probabilidade: 0,5 = 50% = 1/2
+        Impossivel: -0,5 / -20% / 2/1
+        
+    Conceitos:
+        Experimento: o que esta sendo estudade.
+        Espaço amostral: conjunto de todos os resultados possíveis de um experimento.
+        Evento: resultados ocorridos.
+        
+        * Exemplo: 
+            - Experimento: lançamento de um dado.
+            - Espaço amostral: {1, 2, 3, 4, 5, 6}
+            - Evento: {1, 3, 5}
+            
+    Eventos:
+        Eventos excludentes: eventos que não podem ocorrer ao mesmo tempo.
+        Eventos não excludentes: eventos que podem ocorrer ao mesmo tempo.
+        Eventos dependentes: eventos que dependem um do outro.
+        Eventos independentes: eventos que não dependem um do outro.
+
+        * Um unico evento: P(A) = n(Ocorrencias esperadas) / n(Eventos possiveis)
+        Exemplo:
+            - Lançamento de um dado, qual a probabilidade de sair 1?
+            - P(A) = 1/6 = 0,1667 = 16,67%
+            
+            - Lançamento de um dado, qual a probabilidade de sair 1 ou 2?
+            - P(A) = 2/6 = 0,3333 = 33,33%
+            
+            - Lançamento de um dado e dar impar ou maior que 4?
+            - P(A) = 4/6 = 0,6667 = 66,67%
+"""
