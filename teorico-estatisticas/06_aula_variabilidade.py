04b88eb40a6c9d039bfb7c9f2da4619ba7bcf979
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:08:08 -0300

    [ADD] add definitions of variability concepts in 06_aula_variabilidade.py

---
+++ teorico-estatisticas/06_aula_variabilidade.py
@@ -0,0 +1,22 @@
+"""
+    Variabilidade:
+        - Variancia
+        - Desvio padrão
+        - Amplitude
+        - Não centrais: Quartis
+        
+
+        * Variancia:
+            Como os dados variam de acordo com a média.
+            
+        * Desvio padrão:
+            É a raiz quadrada da variancia.
+            
+        * Amplitude:
+            Diferença entre o maior e o menor valor.
+            
+        * Quartis:
+            - Divisão dos dados em quatro partes iguais.
+            - Q1, Q2 e Q3
+            - Q2 é a mediana.
+"""
