bb3efb160f3e00d03614b2862d631697d944e57e
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:07:54 -0300

    [ADD] add sampling concepts and definitions in 03_aula_amostras.py

---
+++ teorico-estatisticas/03_aula_amostras.py
@@ -0,0 +1,34 @@
+"""
+    Amostras:
+        - Parte de uma população, selecionada usando alguma técnica que de chances iguais
+        a todos os elementos da população de serem selecionados.
+        
+    Conceitos:
+        - População: alvo de estudos.
+        - Amostras: subconjunto da população
+        - Censo: pesquisa com toda a população
+        
+    ** Amostra != População
+    
+    ** Porque amostra?
+        - Porque pode ser caro e custoso inferir sobre toda a população(Censo)
+    
+    ** É possivel inferir sobre uma amostra
+        - Uma amostra feita corretamente deve representar as mesmas caracteristicas da 
+        população de onde foi retiradas.
+        - Se ela não representa a população, dizemos que ela é enviesada
+        
+    ** Enviesamento
+        - Ocorre quando se subestima ou superestima o parâmetro da população
+        - Causas: Pesquisa de pessoas próximas ou de fácil acesso, Pesquisa pela internet, sem uso de mecânismo de aleatóriedade.
+    
+    ** Custo da amostra
+        - Margem de erro e nível de confiança.
+        - Variação: amostras disferentes podem representar resultados diferentes.
+        - Podemos medir a variação esperada.
+        
+    ** Mundo real:  
+        - Amplamente utilizada em pesquisa e estudos e etc.
+        - Faz parte do dia a dia de um Cientista de dados.
+    
+"""
