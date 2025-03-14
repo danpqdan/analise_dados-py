e2af3945d7d58d068b9aba1b05378e20638b12c7
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:07:44 -0300

    [ADD] add introductory statistics concepts and definitions in 01_aula_estatisticas.py

---
+++ teorico-estatisticas/01_aula_estatisticas.py
@@ -0,0 +1,42 @@
+"""
+    Estatisticas:
+        Estatisticas é a ciência que se utiliza das teorias probabilisticas
+        para explicar a frequencia de ocorrência de eventos.
+        
+    Principais divisões:
+        - Descritiva
+        - Inferencial
+        - Probabilidade
+        
+    Descritiva:
+        - Coleta
+        - Organização
+        - Resumo
+        - Apresentação
+        - Análise
+        
+    Inferencial:
+        - Estimação
+        - Teste de Hipóteses
+        
+    Probabilidade:
+        - Estudo de fenômenos aleatórios
+        - Modelagem de fenômenos aleatórios
+        - Previsão de eventos futuros
+        
+    Observação e Experimentação:
+        Observação: Estudo em que os elementos analisados não são afetados(Pesquisa)
+        Experimento: Condições ou tratamentos são impostos a grupos, para avaliar o resultado
+        
+    Variáveis:
+        Quantitativas - Numéricas:
+            Contínuas: valores reais, podem assumir qualquer intervalo.
+            Discretas: números fixos, inteiros e num intervalo
+        Qualitativas - Categóricas
+            Nominais: categorias sem hierarquia.
+            Ordinais: categorias com hierarquia.
+            
+    ** Estatisticas dependem de: Interpretação, Escolhas e Avaliações.
+            
+
+"""
