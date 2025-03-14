18d62f73041447d577803c8d82471b8f0737f864
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:08:04 -0300

    [ADD] add definitions of central tendency measures in 05_aula_centralidade.py

---
+++ teorico-estatisticas/05_aula_centralidade.py
@@ -0,0 +1,18 @@
+"""
+    Centralidade:
+        - Média
+        - Moda
+        - Mediana
+        
+    Média:
+        É a soma de toda a população dividido pelo total de elementos somados.
+        
+    Média ponderada:
+        Multiplica os elementos pelo peso, soma o resultados e divide pelo total de pesos.
+        
+    Moda:
+        Valor que mais ocorre durante a coleta dos dados.
+        
+    Mediana:
+        Em uma busca ordenada a mediana é o indece que se encontra no meio da busca.
+"""
