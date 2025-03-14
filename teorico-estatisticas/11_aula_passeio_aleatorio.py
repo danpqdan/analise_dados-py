f51ccbca34ed3098b8216ee048a695424d0779b4
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:08:49 -0300

    add introduction to random walk theory in 11_aula_passeio_aleatorio.py

---
+++ teorico-estatisticas/11_aula_passeio_aleatorio.py
@@ -0,0 +1,23 @@
+"""
+    Passeio aleatório:
+        - Sucessão de passos aleatórios e independentes.
+        - Usada para modelar movimentos aleatórios.
+        - Em finanças, é usado para modelar o preço de ações.
+        
+    * Não é totalmente aleatório, pois é baseado em probabilidades.
+    * Referencia: The ramdom walk for dummies. Richard A. Monte
+        - O bebado sempre começa no ponto 1.
+        - O bebado sempre vai para frente ou para trás.
+        - O bebado sempre anda uma unidade.
+        - O bebado sempre tem 50% de chance de ir para frente ou para trás.
+        - O bebado tem chance de 50% de dar um passo para direita ou esquerda.
+        - A probabilidade sempre aumenta a medida que o bebado anda.
+        
+    Açoes e investimentos:
+        Duas correntes:
+            - Comportamento das ações são um passeio aleatório, portanto não é possível prever o futuro.
+            - O comportamento das ações não são um passeio aleatório, portanto é possível prever o futuro.
+            
+    
+
+"""
