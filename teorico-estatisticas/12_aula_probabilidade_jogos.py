e595a099ca0de2f1ccc2b10561eb828d11eb0325
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-14 13:08:53 -0300

    add tips for gambling strategies and probability analysis in 12_aula_probabilidade_jogos.py

---
+++ teorico-estatisticas/12_aula_probabilidade_jogos.py
@@ -0,0 +1,22 @@
+"""
+    Dicas para jogos de azar:
+        - Jogar numeros que mais saem.
+        - Jogar numeros que menos saem.
+        - Eliminar combinações sem pares ou sem impares.
+        
+    veja:
+        Sorteios são eventos independentes.
+        A probabilidade de sortear:
+            1, 2, 3, 4, 5, 6 = 1/6 = 16,67%
+            Qualquer combinação = 1/6 = 16,67%
+            
+    Explicação: 
+        Porque na mega sena nunca saiu o numero 1, 2, 3, 4, 5, 6?
+            - 50.063.860 combinações possíveis.
+            - 2 vezes na semana.
+            - 1 ano tem 52 semanas.
+            - 104 sorteios por ano.
+            
+            * Sorteios realizados: ~2500
+            * Supondo sorteios sem reposição levaria 481.123 anos
+"""
